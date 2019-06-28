141369a7a27ceb74c8dc2f928a82e10943096ea6
MrWolfeTV <mrwolfe.tv@gmail.com>
2019-06-27 22:01:04 -0700

    moved wireframe to folder and began actual project

---
+++ index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// current question numbers and score
+let questionNumber = 0;
+let score = 0;
+
+// figure out html and css for the quiz
+
+// create function for button to start and change screens per question and answer
+
+// create function for changing question number
+
+// create function for adding to score
+
+
+// funtion to handle all the buttons
+function init() {
+  handleStart();
+  handleCheck();
+  handleNext();
+  handleRestart();
+}
+
+init();
---
+++ quizdata.js
@@ -0,0 +1,27 @@
+const STORE = [
+    {
+        question: 'How many species of bees are currently known?',
+        answers: ['100', '600', '1600', '16000'],
+        correctAnswer: '16000'
+    },
+    {
+        question: 'How many eyes do bees have?',
+        answers: ['2', '3', '4', '5'],
+        correctAnswer: '5'
+    },
+    {
+        question: 'What species of bees make a honey-like substance from the flesh of a dead animal?',
+        answers: ['Carnivorous Bees?', 'Blood Bees', 'Vulture Bees', 'Meat Bees'],
+        correctAnswer: 'Vulture Bees'
+    },
+    {
+        question: 'Where was ancient art found depicting domestication of bees roughly 4500 years ago?',
+        answers: ['Italy', 'Egypt', 'Mexico', 'China'],
+        correctAnswer: 'Egypt'
+    },
+    {
+        question: 'How much of human food supply depends on pollination?',
+        answers: ['1/3', '1/2', '5/8', '3/4'],
+        correctAnswer: '1/3'
+    }
+];
