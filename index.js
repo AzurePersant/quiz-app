0ec50dc1c6ec66259d389d1b4f6c925876cf05f9
MrWolfeTV <mrwolfe.tv@gmail.com>
2019-07-09 16:48:35 -0700

    added changing scores and question numbers function

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 "use strict";
 
+/*
+Checklist:
+change score
+change question number
+
+
+*/
+
 // current question numbers and score
 let questionNumber = 0;
 let score = 0;
@@ -7,11 +15,42 @@ let score = 0;
 // figure out html and css for the quiz
 
 // create function for button to start and change screens per question and answer
+function handleStart() {
+
+}
+
+function handleNext() {
+
+}
+
+function handleCheck() {
+
+}
+
+function handleRestart() {
+
+}
 
 // create function for changing question number
+function changeQuestionNumber() {
+  // add 1 to question number
+  questionNumber ++;
+  //change the html question number text
+  $('.qNumber').text(questionNumber);
+  //test log
+  //console.log(questionNumber);
+
+}
 
 // create function for adding to score
-
+function changeScore() {
+  // add 1 to score
+  score ++;
+  //change the html score number text
+  $('.score').text(score);
+  //test log
+  //console.log(score);
+}
 
 // funtion to handle all the buttons
 function init() {
